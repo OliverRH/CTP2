76deae88731c04e8ffa4b8f99d70a231be92799b
OliverRH <oliverrosengreenhenriksen@hotmail.com>
2022-05-02 15:36:16 +0200

    fuld fart

--- setup_database_for_CTP2.py
+++ setup_database_for_CTP2.py
@@ -17,7 +17,7 @@ def create_config_file(ip_host, db_user, db_pass, CTP2_db_name, CTP2_db_table_na
                           'CTP2_db_table_name': CTP2_db_table_name,
                           'CTP2_db_table_columns_names': ', '.join(CTP2_db_table_columns_names)}
     
-    with open('config.ini', 'w') as configfile:
+    with open('config_db.ini', 'w+') as configfile:
         config.write(configfile)
 #----------------------------------------------------------------
 
@@ -45,8 +45,8 @@ CTP2_db_table_columns_names = ["Pi_data", "Pi_time"]
 CTP2_db_table_columns_create =   "(" + CTP2_db_table_columns_names[0] + " VARCHAR(100), " + CTP2_db_table_columns_names[1] + " DATETIME)"       
 column_names = CTP2_db_table_columns_names[0] + ", " + CTP2_db_table_columns_names[1]
 
-create_database(ip_host, db_user, db_pass, CTP2_db_name)
-time.sleep(1)
-create_table_in_database(ip_host, db_user, db_pass, CTP2_db_name, CTP2_db_table_name, CTP2_db_table_columns_create)
-time.sleep(1)
+#create_database(ip_host, db_user, db_pass, CTP2_db_name)
+#time.sleep(1)
+#create_table_in_database(ip_host, db_user, db_pass, CTP2_db_name, CTP2_db_table_name, CTP2_db_table_columns_create)
+#time.sleep(1)
 create_config_file(ip_host, db_user, db_pass, CTP2_db_name, CTP2_db_table_name, CTP2_db_table_columns_names)
---
+++ setup_zigbee.py
@@ -0,0 +1,28 @@
+import configparser
+import sys
+
+#Creates config.ini file that contains credentials for database login and information about the database 
+#----------------------------------------------------------------
+config = configparser.ConfigParser()
+    
+def create_config_file(Pi_ip_address, zigbee_subscriber_address, zigbee_publisher_address):
+
+    config['zigbee2mqtt'] = {'Pi_ip_address': Pi_ip_address,
+                             'zigbee_subscriber_address': zigbee_subscriber_address,
+                             'zigbee_publisher_address': zigbee_publisher_address}
+    
+    with open('config_zigbee.ini', 'w+') as configfile:
+        config.write(configfile)
+#----------------------------------------------------------------
+
+print("\n----------------------------------------------------------------")
+print("IP address of the Raspberry Pi:", sys.argv[1])
+print("\nName of the zigbee subscriber address:", sys.argv[2])
+print("\nName of the zigbee publisher address:", sys.argv[3])
+print("----------------------------------------------------------------\n")
+
+Pi_ip_address = sys.argv[1]
+zigbee_subscriber_address = sys.argv[2]
+zigbee_publisher_address = sys.argv[3]
+
+create_config_file(Pi_ip_address, zigbee_subscriber_address, zigbee_publisher_address)
