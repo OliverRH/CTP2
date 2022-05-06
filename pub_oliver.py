55ffbf38523fb959d34c7bd4714a5c9576ac314d
OliverRH <oliverrosengreenhenriksen@hotmail.com>
2022-05-06 14:39:50 +0200

    asd

--- pub_oliver.py
+++ pub_oliver.py
@@ -14,14 +14,14 @@ Pi_ip_port = int(config['zigbee2mqtt']['pi_ip_port'])
 LED_zigbee_addr = config['zigbee2mqtt']['zigbee_publisher_address']
 Sensor_zigbee_addr = config['zigbee2mqtt']['zigbee_subscriber_address']
 
-payload = """{"battery":100,"illuminance":839,"illuminance_lux":839,"linkquality":141,"occupancy":true,"temperature":25,"voltage":3025}"""
+payload = '{"battery":100,"illuminance":839,"illuminance_lux":839,"linkquality":141,"occupancy":true,"temperature":25,"voltage":3025}'
 
 json_str = json.loads(payload)
 print(json_str)
 
 client = mqtt.Client()
 client.connect(Pi_ip_address, Pi_ip_port) 
-client.publish(Sensor_zigbee_addr, json_str) 
+client.publish(Sensor_zigbee_addr, str(json_str)) 
 client.disconnect()
 
 
