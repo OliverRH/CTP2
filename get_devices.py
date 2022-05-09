271a92784b0201a21e641ae15975eaf8fce43a13
OliverRH <oliverrosengreenhenriksen@hotmail.com>
2022-05-09 16:55:55 +0200

    af

--- get_devices.py
+++ get_devices.py
@@ -4,8 +4,16 @@ from yaml.loader import FullLoader
 with open('/opt/zigbee2mqtt/data/configuration.yaml') as f:
     data = yaml.load(f, Loader=FullLoader)
     print("TEST")
-    print(data['devices'])
+    #print(data['devices'])
     
+    devices = data['devices']
+    
+    for device in devices:
+        print(device)
+    
+    
+    """
     print('After Sorting')
     sorted_data = yaml.dump(data, sort_keys=True)
     print(sorted_data)
+    """
